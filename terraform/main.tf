24b5786e0cca6bb8feb0ad83ce29fa3a72b746d1
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2025-06-14 14:45:25 +0200

    Adding things in a two-step process

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,22 +25,22 @@ resource "azurerm_dns_zone" "dns_zone" {
   resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
 }
 
-resource "azurerm_static_web_app_custom_domain" "example" {
+resource "azurerm_static_web_app_custom_domain" "custom_domain" {
   static_web_app_id = azurerm_static_web_app.primary_static_web_app.id
   domain_name       = local.dns_zone_resource_name
   validation_type   = "dns-txt-token"
 }
 
-resource "azurerm_dns_txt_record" "example" {
-  count               = azurerm_static_web_app_custom_domain.example.validation_token == null || azurerm_static_web_app_custom_domain.example.validation_token == "" ? 0 : 1
-  name                = "_dnsauth"
-  zone_name           = azurerm_dns_zone.dns_zone.name
-  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
-  ttl                 = 300
-  record {
-    value = azurerm_static_web_app_custom_domain.example.validation_token
-  }
-}
+# resource "azurerm_dns_txt_record" "example" {
+#   count               = azurerm_static_web_app_custom_domain.custom_domain.validation_token == null || azurerm_static_web_app_custom_domain.custom_domain.validation_token == "" ? 0 : 1
+#   name                = "_dnsauth"
+#   zone_name           = azurerm_dns_zone.dns_zone.name
+#   resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+#   ttl                 = 300
+#   record {
+#     value = azurerm_static_web_app_custom_domain.custom_domain.validation_token
+#   }
+# }
 
 resource "azurerm_dns_a_record" "example" {
   name                = "@"
