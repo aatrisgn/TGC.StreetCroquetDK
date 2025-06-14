78edce1a3f2becf31457a00844eaaf8617905206
Asger Arnbjørn Thyregod <asger.thyregod@gmail.com>
2025-06-14 14:40:45 +0200

    Creating DNS zone for domain handling

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,4 @@
 locals {
   resource_location_name = replace(lower(data.azurerm_resource_group.predefined_resource_group.location), " ", "")
+  dns_zone_resource_name = "streetcrocket.com"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -20,8 +20,32 @@ resource "azurerm_static_web_app" "primary_static_web_app" {
   sku_size            = "Free"
 }
 
-resource "azurerm_static_web_app_custom_domain" "custom_domain" {
+resource "azurerm_dns_zone" "dns_zone" {
+  name                = local.dns_zone_resource_name
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+}
+
+resource "azurerm_static_web_app_custom_domain" "example" {
   static_web_app_id = azurerm_static_web_app.primary_static_web_app.id
-  domain_name       = "streetcrocket.com"
-  validation_type   = "cname-delegation"
+  domain_name       = local.dns_zone_resource_name
+  validation_type   = "dns-txt-token"
+}
+
+resource "azurerm_dns_txt_record" "example" {
+  count               = azurerm_static_web_app_custom_domain.example.validation_token == null || azurerm_static_web_app_custom_domain.example.validation_token == "" ? 0 : 1
+  name                = "_dnsauth"
+  zone_name           = azurerm_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+  ttl                 = 300
+  record {
+    value = azurerm_static_web_app_custom_domain.example.validation_token
+  }
+}
+
+resource "azurerm_dns_a_record" "example" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.dns_zone.name
+  resource_group_name = data.azurerm_resource_group.predefined_resource_group.name
+  ttl                 = 300
+  target_resource_id  = azurerm_static_web_app.primary_static_web_app.id
 }
