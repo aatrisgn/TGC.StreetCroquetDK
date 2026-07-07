8d207a6df98dfda8896b3c670a593b47b75fb2b0
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-07-07 20:48:48 +0200

    Adjusted

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -1,7 +1,7 @@
 resource "scaleway_domain_record" "a_records" {
   for_each = toset(local.cluster_domains)
 
-  dns_zone = "asgerthyregod.dk"
+  dns_zone = "streetcrocket.com"
   name     = each.value
   type     = "A"
   data     = "51.15.251.138"
