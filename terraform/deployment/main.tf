b1ac11530e8bd775d961e22c00ea54a21f63f296
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-07-13 19:52:50 +0200

    Changed path

--- terraform/deployment/main.tf
+++ terraform/deployment/main.tf
@@ -1,5 +1,5 @@
 module "deployment_files" {
-  source = "git::https://github.com/aatrisgn/TGC.templates/terraform/modules/deployment_file.git?ref=main"
+  source = "git::https://github.com/aatrisgn/TGC.templates.git//terraform//modules//deployment_file?ref=main"
   for_each = { for val in local.deployment_files : val => val }
 
   source_path = "${var.source_path}"
